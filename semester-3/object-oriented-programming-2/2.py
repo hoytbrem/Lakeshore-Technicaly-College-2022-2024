c60e13b5147643382111e4422296618bb2b38724
Hoyt Brem <104306396+hoytbrem@users.noreply.github.com>
2023-10-31 15:13:07 -0500

    Delete semester-3/object-oriented-programming-2/2.py

--- semester-3/object-oriented-programming-2/2.py
+++
@@ -1,9 +0,0 @@
-base = int(input("Enter the base of the triangle: "))
-
-height = int(input("Enter the height of the triangle: "))
-
-area = 0.5 * base * height
-
-print("The area is", area, "Square units.")
-
-# will caclulate the area of the triangle and will also fail if its the wrong data type as well
