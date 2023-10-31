6039d4f0dfb38d80cf9a9a39a3fd6f310582c96e
Hoyt Brem <104306396+hoytbrem@users.noreply.github.com>
2023-10-31 15:13:00 -0500

    Delete semester-3/object-oriented-programming-2/1.py

--- semester-3/object-oriented-programming-2/1.py
+++
@@ -1,9 +0,0 @@
-width = int(input("Enter the width: "))
-
-height = int(input("Enter the height: "))
-
-area = width * height
-
-print("The area is", area, "Square units.")
-
-# learned that it will only accept int value. otherwise my vscode literally explodes because i think i configured this thing wrong but im too lazy to fix it 
