7564c5f3d1af9d5519000e2073d4ccaedc1a2133
Hoyt Brem <hoytbrem@gmail.com>
2023-10-31 19:24:56 -0500

    add

--- semester-3/object-oriented-programming-2/week4/2.py
+++ semester-3/object-oriented-programming-2/week4/2.py
@@ -7,4 +7,33 @@ the number of lines in the file. If the input is 0, the program quits. Otherwise
 program prints the line associated with that number.
 """
 
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
+        return lines
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return []
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 file_name = input("Input the file name: ")
+
+lines = read_file(file_name)
+
+num_lines = len(lines)
+
+while True:
+    print(f"\nNumber of lines in the file: {num_lines}")
+    line_number = int(input("Enter a line number (1 to N, or 0 to quit): "))
+
+    if line_number == 0:
+        break
+    elif 1 <= line_number <= num_lines:
+        print(f"Line {line_number}: {lines[line_number - 1]}")
+    else:
+        print(f"Invalid line number. Please enter a number between 1 and {num_lines}.")
--- semester-3/object-oriented-programming-2/week4/3.py
+++ semester-3/object-oriented-programming-2/week4/3.py
@@ -2,3 +2,41 @@
 7. Write a program that inputs a text file. The program should print the unique
 words in the file in alphabetical order.
 """
+
+
+def get_unique_words(filename):
+    # uses set because it doesnt allow duplicates
+    unique_words = set()
+
+    try:
+        # opens a file
+        with open(filename, "r") as file:
+            # goes through each line
+            for line in file:
+                # splits where there are spaces
+                words = line.split()
+
+                # for each of those words in the line it does this stuff
+                for word in words:
+                    # gets rid of all special characters and sends it all to lowercase
+                    word = word.strip('.,!?";:').lower()
+                    # adds the words to the set and if its a duplicate it deletes it
+                    unique_words.add(word)
+    # if the file input failed it'll shoot an error
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+
+    # it will sort the set and return it
+    return sorted(unique_words)
+
+
+filename = input("Enter the filename: ")
+unique_words = get_unique_words(filename)
+
+# if unique_words isnt null itll do this
+if unique_words:
+    print("Unique words in alphabetical order:")
+
+    # prints everysingle word
+    for word in unique_words:
+        print(word)
